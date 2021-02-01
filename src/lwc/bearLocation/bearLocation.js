c0afadcd56a7fd2f81fa438c14c63795c43ebf1a
Marina Rezantseva <34715528+geordanygold@users.noreply.github.com>
2021-02-01 15:48:05 +0200

    first commit

---
+++ src/lwc/bearLocation/bearLocation.js
@@ -0,0 +1,9 @@
+/**
+ * Created by marynariezantseva on 2019-08-01.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class BearLocation extends LightningElement {
+
+}
---
+++ src/lwc/helloWebComponent/helloWebComponent.js
@@ -0,0 +1,9 @@
+/**
+ * Created by marynariezantseva on 2019-08-01.
+ */
+
+import { LightningElement, track } from 'lwc';
+
+export default class HelloWebComponent extends LightningElement {
+    @track greeting = 'Trailblazer';
+}
