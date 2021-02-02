52a59e1928e6673b5b350c2e0906a3d6a0dfda41
Marina Rezantseva <mrezantseva@gmail.com>
2021-02-02 17:33:55 +0200

    Added button to test_component_3

---
+++ src/lwc/test_component_3/test_component_3.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Марина on 01.02.2021.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class TestComponent3 extends LightningElement {
+
+    // DMITRY LEE'O & NILS HOLGERSON - GLUBOKO (DUB MIX)
+
+}
